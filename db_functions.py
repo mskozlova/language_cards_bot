419a0359635c181893f817d6442f7f84776b9339
Maria Kozlova <mariakozlova@192.168.1.157>
2023-05-17 19:49:33 +0100

    transfer train to register_next_step_handler: less ydb queries, make prettier

--- db_functions.py
+++ db_functions.py
@@ -268,25 +268,7 @@ def get_current_language(pool, chat_id):
     return pool.retry_operation_sync(callee)
 
 
-def get_current_session_id(pool, chat_id):
-    def callee(session):
-        result_sets = session.transaction().execute(
-            """
-            SELECT session_id
-            FROM `{}`
-            WHERE chat_id == {}
-            """.format(
-                USERS_TABLE_PATH, chat_id
-            ),
-            commit_tx=True,
-        )
-        raw_result = result_sets[0].rows
-        return raw_result[0]["session_id"] if len(raw_result) == 1 else None
-
-    return pool.retry_operation_sync(callee)
-
-
-def init_training_session(pool, chat_id, language, session_id, strategy):
+def init_training_session(pool, chat_id, session_info):
     def callee(session):
         session.transaction().execute(
             """
@@ -294,47 +276,21 @@ def init_training_session(pool, chat_id, language, session_id, strategy):
             $session_id = {};
             $strategy = "{}";
             $language = "{}";
+            $direction = "{}";
+            $duration = {};
+            $hints = "{}";
             
             UPDATE `users`
             SET session_id = $session_id
             WHERE chat_id == $chat_id;
             
-            UPSERT INTO `training_session_info` (`chat_id`, `session_id`, `strategy`, `language`) VALUES
-            ($chat_id, $session_id, $strategy, $language)
+            UPSERT INTO `training_session_info`
+            (`chat_id`, `session_id`, `strategy`, `language`, `direction`, `duration`, `hints`) VALUES
+            ($chat_id, $session_id, $strategy, $language, $direction, $duration, $hints)
             """.format(
-                chat_id, session_id, strategy, language
-            ),
-            commit_tx=True,
-        )
-
-    return pool.retry_operation_sync(callee)
-
-
-def set_session_order_and_length(pool, chat_id, session_id, order, length):
-    def callee(session):
-        session.transaction().execute(
-            """
-            UPDATE `{}`
-            SET order = "{}", length = {}
-            WHERE chat_id == {} AND session_id == {};
-            """.format(
-                TRAINING_SESSIONS_INFO_TABLE_PATH, order, length, chat_id, session_id
-            ),
-            commit_tx=True,
-        )
-
-    return pool.retry_operation_sync(callee)
-
-
-def set_session_hints(pool, chat_id, session_id, hints):
-    def callee(session):
-        session.transaction().execute(
-            """
-            UPDATE `{}`
-            SET hints = "{}", current_word_idx = 1
-            WHERE session_id == {} AND chat_id == {};
-            """.format(
-                TRAINING_SESSIONS_INFO_TABLE_PATH, hints, session_id, chat_id
+                chat_id, session_info["session_id"],
+                session_info["strategy"], session_info["language"],
+                session_info["direction"], session_info["duration"], session_info["hints"]
             ),
             commit_tx=True,
         )
@@ -358,8 +314,7 @@ def get_session_info(pool, chat_id, session_id):
 
     return pool.retry_operation_sync(callee)
 
-
-def create_training_session(pool, chat_id, session_id, current_lang, length, strategy, order):
+def create_training_session(pool, chat_id, session_info):
     def callee(session):
         session.transaction().execute(
             """
@@ -369,62 +324,41 @@ def create_training_session(pool, chat_id, session_id, current_lang, length, str
             $length = {};
             $strategy = "{}";
             $order = "{}";
-
-            UPSERT INTO `training_sessions`
-            SELECT
-                $chat_id AS chat_id,
-                $session_id AS session_id,
-                word,
-                ROW_NUMBER() OVER w AS word_idx,
-            FROM `vocabularies`
-            WHERE
-                chat_id == $chat_id
-                AND language == $language
-                AND CASE
-                    WHEN $strategy == "new" AND $order == "to"
-                        THEN NVL(n_trains_to, 0) <= 2
-                    WHEN $strategy == "new" AND $order == "from"
-                        THEN NVL(n_trains_from, 0) <= 2
-                    WHEN $strategy == "bad" AND $order == "to"
-                        THEN score_to <= 0.7
-                    WHEN $strategy == "bad" AND $order == "from"
-                        THEN score_from <= 0.7
-                    ELSE True
-                END
-            WINDOW w AS (
-                ORDER BY RandomNumber(CAST($session_id AS String) || word)
-            )
-            LIMIT $length;
-            """.format(
-                session_id, chat_id, current_lang, length, strategy, order
-            ),
-            commit_tx=True,
-        )
-
-    return pool.retry_operation_sync(callee)
-
-
-def update_length(pool, chat_id, session_id):
-    def callee(session):
-        session.transaction().execute(
-            """
-            $chat_id = {};
-            $session_id = {};
             
-            $length = (
+            $words_sample = (
                 SELECT
-                    chat_id,
-                    session_id,
-                    COUNT(*) AS length,
-                FROM `training_sessions`
-                WHERE chat_id == $chat_id AND session_id == $session_id
-                GROUP BY chat_id, session_id
+                    $chat_id AS chat_id,
+                    $session_id AS session_id,
+                    word,
+                    translation,
+                    ROW_NUMBER() OVER w AS word_idx,
+                FROM `{}`
+                WHERE
+                    chat_id == $chat_id
+                    AND language == $language
+                    AND CASE
+                        WHEN $strategy == "new" AND $order == "to"
+                            THEN NVL(n_trains_to, 0) <= 2
+                        WHEN $strategy == "new" AND $order == "from"
+                            THEN NVL(n_trains_from, 0) <= 2
+                        WHEN $strategy == "bad" AND $order == "to"
+                            THEN n_trains_to >= 1 AND 1.0 * score_to / n_trains_to <= 0.7
+                        WHEN $strategy == "bad" AND $order == "from"
+                            THEN n_trains_from >= 1 AND 1.0 * score_from / n_trains_from <= 0.7
+                        ELSE True
+                    END
+                WINDOW w AS (
+                    ORDER BY RandomNumber(CAST($session_id AS String) || word)
+                )
             );
-            
-            UPDATE `training_session_info` ON
-            SELECT * FROM $length;
+
+            UPSERT INTO `{}`
+            SELECT * FROM $words_sample
+            WHERE word_idx <= $length;
             """.format(
-                chat_id, session_id
+                session_info["session_id"], chat_id, session_info["language"], session_info["duration"],
+                session_info["strategy"], session_info["direction"],
+                VOCABS_TABLE_PATH, TRAINING_SESSIONS_TABLE_PATH
             ),
             commit_tx=True,
         )
@@ -432,63 +366,21 @@ def update_length(pool, chat_id, session_id):
     return pool.retry_operation_sync(callee)
 
 
-def get_next_word(pool, chat_id, session_id):
+def get_training_words(pool, chat_id, session_info):
     def callee(session):
         result_sets = session.transaction().execute(
             """
-            $chat_id = {};
             $session_id = {};
-            
-            SELECT
-                v.word AS word,
-                v.translation AS translation,
-                tsi.`order` AS `order`,
-                tsi.current_word_idx AS current_word_idx,
-                tsi.length AS length,
-            FROM `training_session_info` AS tsi
-            INNER JOIN `training_sessions` AS ts ON
-                tsi.session_id == ts.session_id
-                AND tsi.current_word_idx == ts.word_idx
-            INNER JOIN `vocabularies` AS v ON
-                tsi.language == v.language
-                AND ts.word == v.word
-            WHERE
-                tsi.chat_id == $chat_id
-                AND tsi.session_id == $session_id
-            """.format(
-                chat_id, session_id
-            ),
-            commit_tx=True,
-        )
-        return result_sets[0].rows[0]
-
-    return pool.retry_operation_sync(callee)
-
-
-def get_test_hints(pool, chat_id, session_id, word, language):
-    def callee(session):
-        result_sets = session.transaction().execute(
-            """
             $chat_id = {};
-            $session_id = {};
-            $word = "{}";
-            $language = "{}";
-            
-            SELECT
-                word,
-                translation,
-                ROW_NUMBER() OVER w AS hint_idx,
-            FROM `vocabularies`
+
+            SELECT * FROM `{}`
             WHERE
-                chat_id == $chat_id
-                AND language == $language
-                AND word != $word
-            WINDOW w AS (
-                ORDER BY RandomNumber(CAST($session_id AS String) || $word || word)
-            )
-            LIMIT 3
+                session_id == $session_id
+                AND chat_id == $chat_id
+            ORDER BY word_idx;
             """.format(
-                chat_id, session_id, word, language
+                session_info["session_id"], chat_id,
+                TRAINING_SESSIONS_TABLE_PATH
             ),
             commit_tx=True,
         )
@@ -497,173 +389,101 @@ def get_test_hints(pool, chat_id, session_id, word, language):
     return pool.retry_operation_sync(callee)
 
 
-def update_score(pool, chat_id, session_id, score):
+def set_training_scores(pool, chat_id, session_id, word_idxs, scores):
     def callee(session):
         session.transaction().execute(
             """
             $chat_id = {};
             $session_id = {};
-            $score = {};
-            
-            $update_training_sessions = (
+            $word_idxs = {};
+            $scores = {};
+
+            $new_scores = (
                 SELECT
-                    chat_id,
-                    session_id,
-                    current_word_idx AS word_idx,
-                    $score AS score,
-                FROM `training_session_info`
-                WHERE
-                    chat_id == $chat_id
-                    AND session_id == $session_id
+                    $chat_id AS chat_id,
+                    $session_id AS session_id,
+                    ListZip($word_idxs, $scores) AS scores,
             );
-            
-            $update_training_session_info = (
-                SELECT
-                    chat_id,
-                    session_id,
-                    current_word_idx + 1 AS current_word_idx,
-                FROM `training_session_info`
-                WHERE
-                    chat_id == $chat_id
-                    AND session_id == $session_id
-            );
-            
-            UPDATE `training_sessions` ON
-            SELECT * FROM $update_training_sessions;
-            
-            UPDATE `training_session_info` ON
-            SELECT * FROM $update_training_session_info;
-            """.format(
-                chat_id, session_id, score
-            ),
-            commit_tx=True,
-        )
 
-    return pool.retry_operation_sync(callee)
-
-
-def get_scores(pool, chat_id, session_id):
-    def callee(session):
-        result_sets = session.transaction().execute(
-            """
+            UPSERT INTO `{}`
             SELECT
-                COUNT_IF(score IS NOT NULL) AS words,
-                SUM(score) AS successes,
-            FROM `training_sessions`
-            WHERE chat_id == {} AND session_id == {}
+                chat_id,
+                session_id,
+                scores.0 AS word_idx,
+                scores.1 AS score,
+            FROM $new_scores
+            FLATTEN LIST BY scores
             """.format(
-                chat_id, session_id,
+                chat_id, session_id, json.dumps(word_idxs), json.dumps(scores),
+                TRAINING_SESSIONS_TABLE_PATH
             ),
             commit_tx=True,
         )
-        return result_sets[0].rows[0]
 
     return pool.retry_operation_sync(callee)
 
 
-def update_final_scores(pool, chat_id, session_id):
+def update_final_scores(pool, chat_id, session_info):
     def callee(session):
         session.transaction().execute(
             """
             $chat_id = {};
             $session_id = {};
-            
+            $language = "{}";
+            $order = "{}";
+
             $format_dttm = DateTime::Format("%Y-%m-%d %H:%M:%S");
             $get_dttm = ($session_id) -> (
                 $format_dttm(DateTime::FromSeconds(
                     CAST($session_id AS Uint32)
                 ))
             );
-            
+
+            $update_score = ($n_trains, $score_before, $current_score) -> (
+                (NVL($score_before * $n_trains, 0) + $current_score) / (NVL($n_trains, 0) + 1)
+            );
+
             $current_words = (
-                SELECT word
-                FROM `training_sessions`
-                WHERE session_id == $session_id
-            );
-            
-            $all_scores = (
                 SELECT
-                    cw.word AS word,
-                    score,
-                    `order`,
-                    tsi.language AS language,
-                    ts.session_id AS session_id,
-                    ROW_NUMBER() OVER w AS recent_idx,
-                    COUNT(*) OVER m AS total_trains,
-                FROM $current_words AS cw
-                INNER JOIN `training_sessions` AS ts ON
-                    cw.word == ts.word
-                INNER JOIN `training_session_info` AS tsi ON
-                    ts.session_id == tsi.session_id
-                WHERE score IS NOT NULL
-                WINDOW w AS (
-                    PARTITION BY ts.word, tsi.`order`, tsi.language
-                    ORDER BY ts.session_id DESC
-                ), m AS (
-                    PARTITION BY ts.word, tsi.`order`, tsi.language
-                    ORDER BY ts.session_id DESC
-                    ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
-                )
-            );
-            
-            $recent_scores = (
-                SELECT
-                    language,
                     word,
-                    $chat_id AS chat_id,
-                    MAX(IF(`order` == "to", $get_dttm(session_id))) AS last_train_to,
-                    MAX(IF(`order` == "from", $get_dttm(session_id))) AS last_train_from,
-                    MAX(IF(`order` == "to", total_trains)) AS n_trains_to,
-                    MAX(IF(`order` == "from", total_trains)) AS n_trains_from,
-                    CAST(AVG_IF(score, `order` == "to") AS Float?) AS score_to,
-                    CAST(AVG_IF(score, `order` == "from") AS Float?) AS score_from,
-                FROM $all_scores
-                WHERE recent_idx <= 10
-                GROUP BY
-                    language,
-                    word
+                    score,
+                FROM `training_sessions`
+                WHERE
+                    chat_id == $chat_id
+                    AND session_id == $session_id
             );
-            
+
+            $new_scores = (
+                SELECT
+                    v.*,
+                    IF($order == "to", $get_dttm($session_id), v.last_train_to) AS last_train_to,
+                    IF($order == "from", $get_dttm($session_id), v.last_train_from) AS last_train_from,
+                    IF($order == "to", NVL(v.n_trains_to, 0) + 1, v.n_trains_to) AS n_trains_to,
+                    IF($order == "from", NVL(v.n_trains_from, 0) + 1, v.n_trains_from) AS n_trains_from,
+                    IF($order == "to", NVL(v.score_to, 0) + 1, v.score_to) AS score_to,
+                    IF($order == "from", NVL(v.score_from, 0) + 1, v.score_from) AS score_from,
+                WITHOUT
+                    v.last_train_from,
+                    v.last_train_to,
+                    v.n_trains_from,
+                    v.n_trains_to,
+                    v.score_from,
+                    v.score_to
+                FROM `vocabularies` AS v
+                INNER JOIN $current_words AS cw ON v.word == cw.word
+                WHERE
+                    v.chat_id == $chat_id
+                    AND v.language == $language
+            );
+
             UPDATE `vocabularies` ON
-            SELECT * FROM $recent_scores;
-            
+            SELECT * FROM $new_scores;
+
             UPDATE `users`
             SET session_id = NULL
             WHERE chat_id == $chat_id;
             """.format(
-                chat_id, session_id
-            ),
-            commit_tx=True,
-        )
-
-    return pool.retry_operation_sync(callee)
-
-
-def cleanup_scores(pool, chat_id):
-    def callee(session):
-        session.transaction().execute(
-            """
-            $chat_id = {};
-            
-            $to_delete = (
-                SELECT
-                    ts.chat_id AS chat_id,
-                    ts.session_id AS session_id,
-                    ts.word_idx AS word_idx,
-                FROM `training_sessions` AS ts
-                LEFT JOIN `training_session_info` AS tsi USING (chat_id, session_id)
-                WHERE
-                    ts.chat_id = $chat_id
-                    AND (
-                        tsi.current_word_idx <= tsi.`length`
-                        OR tsi.session_id IS NULL
-                    )
-            );
-            
-            DELETE FROM `training_sessions` ON
-            SELECT * FROM $to_delete;
-            """.format(
-                chat_id
+                chat_id, session_info["session_id"], session_info["language"], session_info["direction"]
             ),
             commit_tx=True,
         )
